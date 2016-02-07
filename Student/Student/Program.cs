fdb68a5ae6a3195ce3e0aff0654ebfdfcd6c6c79
otegenovaAlt <otegenovaalt@gmail.com>
2016-02-07 17:42:41 +0600

    Student

---
+++ Student/Student/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public double gpa;
+
+        public Student(string name1, string surname1, double gpa1)
+        {
+            this.name = name1;
+            this.surname = surname1;
+            this.gpa = gpa1;
+        }
+        public override string ToString()
+        {
+            return name + " " + surname + " " +gpa;
+        }
+    }
+        class Program
+        { 
+
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            double gpa =Convert.ToDouble(Console.ReadLine());
+            Student info = new Student(name, surname, gpa);
+            Console.WriteLine(info);
+            Console.ReadKey();
+        }
+    }
+}
