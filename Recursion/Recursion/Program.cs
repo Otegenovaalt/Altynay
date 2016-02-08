f37bc963adf92b5d37b32ddbe1bec0f56ebe4716
otegenovaAlt <otegenovaalt@gmail.com>
2016-02-08 19:09:39 +0600

    Stack with  recursion

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree(@"C:\Users\admin\Desktop\Доки 2");
+
+        }
+        static void Tree(string path)
+        {
+            Stack<string> st = new Stack<string>();
+            Console.WriteLine(path + ":" + Directory.GetFiles(path).Length);
+            st.Push(path);
+
+            while (st.Count > 0)
+            {
+                string[] dir = Directory.GetDirectories(st.Pop());
+                foreach (string s in dir)
+                {
+                    Console.WriteLine(s + ":" + Directory.GetFiles(s).Length);
+                    st.Push(s);
+                    Console.ReadKey();
+                }
+
+
+            }
+        }
+
+    }
+}
